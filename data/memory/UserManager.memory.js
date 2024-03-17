c0a561ab7da13d2ca149f601c914da4d0c1ac7d9
Sofía Viotti <sofi_04_04@hotmail.com>
2024-03-17 14:52:40 -0300

    manejo de archivos

--- data/memory/UserManager.memory.js
+++ data/memory/UserManager.memory.js
@@ -1,34 +1,91 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
 class UserManager {
   static #users = [];
-  create(data) {
-    const user = {
-      id:
-        UserManager.#users.length === 0
-          ? 1
-          : UserManager.#users[UserManager.#users.length - 1].id + 1,
-      foto: data.foto,
+  async create(data) {
+    try {
+      const user = {
+      id: Crypto.randomBytes(12).toString("hex"),
+      foto: data.foto || "https://www.pngplay.com/image/325510",
       email: data.email,
       password: data.password,
-      role: 0,
+      role: data.role,
     };
-    UserManager.#users.push(user);
-    console.log("usuariocreado");
+
+    if (!data.email || !data.password || !data.role) {
+    throw new error ("Usuario no creado.Ingrese todos los datos.")
+  } else {
+    UserManager.#users.push(user)
   }
-  read() {
-    return UserManager.#users;
+} catch(error) {
+  console.log(error)
+ }
+ }
+read()  {
+  try {
+  if (UserManager.#users.length===0){
+    throw new error ("Ingrese nuevamente los datos")
+  } else  {
+
+  return UserManager.#users
   }
+  } catch (error) {
+    console.log(error)
+  }
+}  
+
+readOne (id) {
+  try {
+    const one =  UserManager.#users.find(each=>each.id===id)
+  if (!one) {
+    throw new error ("No existe el usuario")
+  } else {
+    return one 
+  }
+}catch(error) {
+  this.console.log(error)
 }
+}
+destroy (id) {
+  try {
+    this.readOne(id)
+  const filtered = UserManager.#users.filter((each)=>each.id!==id);
+  UserManager.#users = filtered;
+  console.log("Usuario eleminado");
+  }
+}    catch (error) {
+  console.log (error)
+}
+}
+
+
 
 const gestorDeUsuarios = new UserManager();
 gestorDeUsuarios.create({
   foto: "sofia.jpg",
   email: "sofi_04_04@hotmail.com",
   password: "hola1234",
+  role: "adm"
 });
 gestorDeUsuarios.create({
   foto: "roxana.jpg",
   email: "roxana@hotmail.com",
   password: "hola5678",
+  role: "user"
+});
+gestorDeUsuarios.create({
+  foto: "celine.jpg",
+  email: "celine@hotmail.com",
+  password: "hola91011",
+  role: "user"
+});
+gestorDeUsuarios.create({
+  foto: "martin.jpg",
+  email: "martin@hotmail.com",
+  password: "hola1213",
+  role: "user"
 });
 
 console.log(gestorDeUsuarios.read());
+users.destroy(3)
