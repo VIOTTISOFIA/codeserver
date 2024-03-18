00202f10b05f60eba4c18b5e2e562b730d5ab6ee
Sofía Viotti <sofi_04_04@hotmail.com>
2024-03-18 10:58:57 -0300

    crypo

--- data/memory/UserManager.memory.js
+++ data/memory/UserManager.memory.js
@@ -6,86 +6,82 @@ class UserManager {
   async create(data) {
     try {
       const user = {
-      id: Crypto.randomBytes(12).toString("hex"),
-      foto: data.foto || "https://www.pngplay.com/image/325510",
-      email: data.email,
-      password: data.password,
-      role: data.role,
-    };
+        id: crypto.randomBytes(12).toString("hex"),
+        foto: data.foto || "https://www.pngplay.com/image/325510",
+        email: data.email,
+        password: data.password,
+        role: data.role,
+      };
 
-    if (!data.email || !data.password || !data.role) {
-    throw new error ("Usuario no creado.Ingrese todos los datos.")
-  } else {
-    UserManager.#users.push(user)
+      if (!data.email || !data.password || !data.role) {
+        throw new error("Usuario no creado.Ingrese todos los datos.");
+      } else {
+        UserManager.#users.push(user);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
-} catch(error) {
-  console.log(error)
- }
- }
-read()  {
-  try {
-  if (UserManager.#users.length===0){
-    throw new error ("Ingrese nuevamente los datos")
-  } else  {
+  read() {
+    try {
+      if (UserManager.#users.length === 0) {
+        // throw new error("Ingrese nuevamente los datos");
+      } else {
+        return UserManager.#users;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  return UserManager.#users
+  readOne(id) {
+    try {
+      const one = UserManager.#users.find((each) => each.id === id);
+      if (!one) {
+        throw new error("No existe el usuario");
+      } else {
+        return one;
+      }
+    } catch (error) {
+      this.console.log(error);
+    }
   }
-  } catch (error) {
-    console.log(error)
+  destroy(id) {
+    try {
+      this.readOne(id);
+      const filtered = UserManager.#users.filter((each) => each.id !== id);
+      UserManager.#users = filtered;
+      console.log("Usuario eliminado");
+    } catch (error) {
+      console.log(error);
+    }
   }
-}  
-
-readOne (id) {
-  try {
-    const one =  UserManager.#users.find(each=>each.id===id)
-  if (!one) {
-    throw new error ("No existe el usuario")
-  } else {
-    return one 
-  }
-}catch(error) {
-  this.console.log(error)
 }
-}
-destroy (id) {
-  try {
-    this.readOne(id)
-  const filtered = UserManager.#users.filter((each)=>each.id!==id);
-  UserManager.#users = filtered;
-  console.log("Usuario eleminado");
-  }
-}    catch (error) {
-  console.log (error)
-}
-}
-
-
 
 const gestorDeUsuarios = new UserManager();
 gestorDeUsuarios.create({
   foto: "sofia.jpg",
   email: "sofi_04_04@hotmail.com",
   password: "hola1234",
-  role: "adm"
+  role: "adm",
 });
 gestorDeUsuarios.create({
   foto: "roxana.jpg",
   email: "roxana@hotmail.com",
   password: "hola5678",
-  role: "user"
+  role: "user",
 });
 gestorDeUsuarios.create({
   foto: "celine.jpg",
   email: "celine@hotmail.com",
   password: "hola91011",
-  role: "user"
+  role: "user",
 });
 gestorDeUsuarios.create({
   foto: "martin.jpg",
   email: "martin@hotmail.com",
   password: "hola1213",
-  role: "user"
+  role: "user",
 });
 
 console.log(gestorDeUsuarios.read());
-users.destroy(3)
