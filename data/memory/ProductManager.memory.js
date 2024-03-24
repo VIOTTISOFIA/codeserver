fe6f54316e2cf42b3a15cd24fec51164182d748d
RoxGarciaRod <roxyjosimar97@gmail.com>
2024-03-24 20:02:29 -0300

    correccion product memory

--- data/memory/ProductManager.memory.js
+++ data/memory/ProductManager.memory.js
@@ -1,71 +1,3 @@
-
-class ProductManager {
-  static #products = [];
-  create(data) {
-    const product = {
-      id:
-        ProductManager.#products.length === 0
-          ? 1
-          : ProductManager.#products[ProductManager.#products.length - 1].id + 1,
-      title: data.title,
-      photo: data.photo,
-      category: data.category,
-      price: data.price,
-      stock: data.stock
-    };
-    ProductManager.#products.push(product);
-    console.log("Producto creado");
-  }
-
-  read() {
-    return ProductManager.#products;
-  }
-}
-
-const gestorDeProductos = new ProductManager();
-
-gestorDeProductos.create({
-    title: "zapatilla",
-  photo: "zapatilla.jpg",
-  category: "calzado",
-  price: 1500,
-  stock: 1000,
-});
-
-gestorDeProductos.create({
-    title: "remera",
-  photo: "remera.jpg",
-  category: "indumentaria",
-  price: 900,
-  stock: 800,
-});
-
-gestorDeProductos.create({
-    title: "cinturon",
-  photo: "cinturon.jpg",
-  category: "accesorios",
-  price: 750,
-  stock: 15,
-});
-
-gestorDeProductos.create({
-    title: "pantalon",
-  photo: "pantalon.jpg",
-  category: "indumentaria",
-  price: 2500,
-  stock: 600,
-});
-
-gestorDeProductos.create({
-    title: "mochila",
-  photo: "mochila.jpg",
-  category: "accesorios",
-  price: 15000,
-  stock: 250,
-});
-
-console.log(gestorDeProductos.read());
-=======
 const fs = require("fs");
 const crypto = require("crypto");
 const { error } = require("console");
@@ -209,23 +141,26 @@ function prueba() {
 
   console.log("Productos creados:");
   console.log(gestorDeProductos.read());
-  
+
   const productId = "123";
-  
+
   try {
     const deleteProduct = gestorDeProductos.readOne(productId);
     if (deleteProduct) {
       gestorDeProductos.destroy(productId);
-      console.log(`Producto "${deleteProduct.title}" eliminado satisfactoriamente.`);
+      console.log(
+        `Producto "${deleteProduct.title}" eliminado satisfactoriamente.`
+      );
       console.log("Productos después de la eliminación:");
       console.log(gestorDeProductos.read());
     } else {
-      console.log(`El producto "${productId}" no existe en la lista. Intentalo nuevamente`);
+      console.log(
+        `El producto "${productId}" no existe en la lista. Intentalo nuevamente`
+      );
     }
   } catch (error) {
     console.error("Error al intentar eliminar el producto:", error.message);
   }
-
 }
 
 prueba();
