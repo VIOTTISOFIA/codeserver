50416431197b708a58058314ccdd0ab4ec703843
Roxana <109628457+RoxGarciaRod@users.noreply.github.com>
2024-04-04 21:04:16 -0300

    Router products (#16)
    * Sprint2 (#4)
    
    * Sprint1 (#3)
    
    * User (#1)
    
    * users
    
    * user
    
    * products (#2)
    
    ---------
    
    Co-authored-by: Roxana <109628457+RoxGarciaRod@users.noreply.github.com>
    
    * sprint2
    
    * manejo de archivos
    
    * crypo
    
    * funciona el JSON
    
    * readSinThownError
    
    ---------
    
    Co-authored-by: Roxana <109628457+RoxGarciaRod@users.noreply.github.com>
    
    * correccion en products fs/memory (#8)
    
    * Sprint1 (#3)
    
    * User (#1)
    
    * users
    
    * user
    
    * products (#2)
    
    ---------
    
    Co-authored-by: Roxana <109628457+RoxGarciaRod@users.noreply.github.com>
    
    * sprint2
    
    * manejo de archivos
    
    * crypo
    
    * funciona el JSON
    
    * readSinThownError
    
    * correcciones
    
    * correc
    
    * correcionesyserver
    
    * correcciones products
    
    * correcciones en memory de products
    
    ---------
    
    Co-authored-by: VIOTTISOFIA <141794941+VIOTTISOFIA@users.noreply.github.com>
    Co-authored-by: Sofía Viotti <sofi_04_04@hotmail.com>
    
    * Delete data/serverExpress/node_modules directory
    
    * Delete data/serverExpress directory
    
    * serverUser
    
    * correcciondecarpetas
    
    * correcciondecarpetas
    
    * serverUser
    
    * Delete package.json
    
    * Delete package-lock.json
    
    * Delete node_modules directory
    
    * carpetas
    
    * createupdate
    
    * armando rutas de express
    
    * prueba de push
    
    * entrega sprint3
    
    * comento funcion en FS de products
    
    * borro usuarios del json
    
    * corrijo errores en memory de products
    
    * correccion de rama
    
    ---------
    
    Co-authored-by: VIOTTISOFIA <141794941+VIOTTISOFIA@users.noreply.github.com>
    Co-authored-by: Sofía Viotti <sofi_04_04@hotmail.com>

---
+++ data/fs/UserManager.fs.js
@@ -0,0 +1,128 @@
+import fs from "fs";
+import crypto from "crypto";
+
+class UserManager {
+  constructor() {
+    this.path = "./data/fs/file/user.json";
+    this.init();
+  }
+  init() {
+    const exists = fs.existsSync(this.path);
+    if (!exists) {
+      const stringData = JSON.stringify([], null, 2);
+      fs.writeFileSync(this.path, stringData);
+      console.log("archivo creado!");
+    } else {
+      console.log("archivo ya existe!");
+    }
+  }
+
+  async create(data) {
+    try {
+      const user = {
+        id: crypto.randomBytes(12).toString("hex"),
+        foto: data.foto || "https://www.pngplay.com/image/325510",
+        email: data.email,
+        password: data.password,
+        role: data.role,
+      };
+
+      if (!data.email || !data.password || !data.role) {
+        throw new Error("Usuario no creado.Ingrese todos los datos.");
+      } else {
+        // creo el objeto con los datos de la nota
+        let users = await fs.promises.readFile(this.path, "utf-8");
+        // espero la lectura del archivo y lo guardo en la variable all
+        users = JSON.parse(users);
+        // parseo
+        users.push(user);
+        // pusheo
+        console.log("usuario creado");
+        users = JSON.stringify(users, null, 2);
+        await fs.promises.writeFile(this.path, users);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async read(rol) {
+    try {
+      let users = await fs.promises.readFile(this.path, "utf-8");
+      // espero la lectura del archivo y lo guardo en la variable users
+      users = JSON.parse(users);
+      // parseo
+      users = users.filter((each) => each.role === rol);
+      if (users.length === 0) {
+        //  si no hay notas
+        // throw new Error("no hay usuarios");
+        return null;
+      } else {
+        console.log(users);
+        return users;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async readOne(id) {
+    try {
+      let users = await fs.promises.readFile(this.path, "utf-8");
+      users = JSON.parse(users);
+      let one = users.find((each) => each.id === id);
+      if (!one) {
+        throw new Error("No existe el usuario");
+      } else {
+        return one;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async destroy(id) {
+    try {
+      let users = await fs.promises.readFile(this.path, "utf-8");
+      users = JSON.parse(users);
+      let filtered = users.filter((each) => each.id !== id);
+      filtered = JSON.stringify(filtered, null, 2);
+      await fs.promises.writeFile(filtered);
+      console.log("Usuario eleminado");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+const User = new UserManager();
+
+async function test() {
+  const gestorDeUsuarios = new UserManager();
+  await gestorDeUsuarios.create({
+    foto: "sofia.jpg",
+    email: "sofi_04_04@hotmail.com",
+    password: "hola1234",
+    role: "adm",
+  });
+  await gestorDeUsuarios.create({
+    foto: "roxana.jpg",
+    email: "roxana@hotmail.com",
+    password: "hola5678",
+    role: "user",
+  });
+  await gestorDeUsuarios.create({
+    foto: "celine.jpg",
+    email: "celine@hotmail.com",
+    password: "hola91011",
+    role: "user",
+  });
+  await gestorDeUsuarios.create({
+    foto: "martin.jpg",
+    email: "martin@hotmail.com",
+    password: "hola1213",
+    role: "user",
+  });
+  console.log(await gestorDeUsuarios.read());
+  console.log(await gestorDeUsuarios.readOne("950ffdebf54f79300a3c7328"));
+}
+// test();
+const userManager = new UserManager();
+export default userManager;
---
+++ server.js
@@ -0,0 +1,177 @@
+import express from "express";
+import userManager from "./data/fs/UserManager.fs.js";
+import productManager from "./data/fs/ProductManager.fs.js";
+const server = express();
+const port = 8080;
+const ready = () => console.log("server ready on port" + port);
+
+server.listen(port, ready);
+
+// middlewares
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+// router
+server.get("/", async (req, res) => {
+  try {
+    return res.status(200).json({
+      statusCode: 200,
+      message: "coder api OK",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      statusCode: 500,
+      message: "coder api ERROR",
+    });
+  }
+});
+
+const create = async (req, res) => {
+  try {
+    const data = req.body;
+    const one = await userManager.create(data);
+    return res.json({
+      statusCode: 201,
+      message: "CREATED ID: " + one.id,
+    });
+  } catch (error) {
+    return res.json({
+      statusCode: error.statusCode || 500,
+      message: error.message || "coder api ERROR",
+    });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const { uid } = req.params;
+    const data = req.body;
+    const one = await userManager.update(uid, data);
+    return res.json({
+      statusCode: 200,
+      message: "UPDATE ID: " + one.id,
+    });
+  } catch (error) {
+    return res.json({
+      statusCode: error.statusCode || 500,
+      message: error.message || "coder api ERROR",
+    });
+  }
+};
+server.post("/api/users", create);
+server.put("/api/users/:uid", update);
+// otra ruta para leer todos
+// luego mando una consulta para hacer un filtro
+server.get("/api/users", async (req, res) => {
+  try {
+    const { role } = req.query;
+    const users = await userManager.read(role);
+    if (users) {
+      return res.status(200).json({
+        response: users,
+        role,
+        success: true,
+      });
+    } else {
+      const error = new Error("not found");
+      error.status = 404;
+      throw error;
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(error.status).json({
+      response: error.message,
+      success: false,
+      // message: "Usuario no existente",
+    });
+  }
+});
+
+//un parametro
+server.get("/api/users/:uid", async (req, res) => {
+  try {
+    const { uid } = req.params;
+    const one = await userManager.readOne(uid);
+    if (one) {
+      return res.status(200).json({
+        response: one,
+        success: true,
+      });
+    } else {
+      const error = new Error("not found");
+      error.statusCode = 404;
+      throw error;
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(error.statusCode).json({
+      response: error.message,
+      success: false,
+    });
+  }
+});
+
+server.get("/api/products/:pid", async (req, res) => {
+  try {
+    const { pid } = req.params;
+    const one = await productManager.readOne(pid);
+    if (one) {
+      return res.status(200).json({
+        response: one,
+        success: true,
+      });
+    } else {
+      const error = new Error("NOT FOUND");
+      error.statusCode = 404;
+      throw error;
+    }
+    
+  } catch (error) {
+    console.log(error);
+    return res.status(error.statusCode).json({
+      response: error.message,
+      success: false,
+    });
+  }
+});
+
+// dos parametro
+
+server.get("/api/users/:id/:role", async (req, res) => {
+  try {
+    const { id, role } = req.params;
+    const data = { id, role };
+    const one = await userManager.create(data);
+    if (one) {
+      return res.status(200).json({
+        response: one,
+        success: true,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(error.statusCode).json({
+      response: error.message,
+      succcess: false,
+    });
+  }
+});
+
+server.get("/api/products/:id/:category", async (req, res) => {
+  try {
+    const { id, category } = req.params;
+    const data = { id, category };
+    const one = await productManager.create(data);
+    return res.status(201).json({
+      response: one,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(error.statusCode).json({
+      response: error.message,
+      success: false,
+    });
+  }
+});
