cf8c87b1ddfdd4aeee715877c87f0ecf7bdd8087
Sofía Viotti <sofi_04_04@hotmail.com>
2024-04-12 20:08:02 -0400

    handlebars

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 import userManager from "./src/data/fs/UserManager.fs.js";
 import errorHandler from "./src/middlewares/errorHandler.mid.js";
 import pathHandler from "./src/middlewares/pathHandler.mid.js";
+
+import { engine } from "express-handlebars";
+
+import __dirname from "./utils.js";
 
 import indexRouter from "./src/router/index.router.js";
 const server = express();
@@ -10,14 +15,17 @@ const ready = () => console.log("server ready on port" + port);
 
 server.listen(port, ready);
 
+server.engine("handlebars", engine());
+server.set("view engine", "handlebars");
+server.set("views", __dirname + "/src/views");
 // middlewares
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
-
 // endpoints
 server.use("/", indexRouter);
 server.use(errorHandler);
 server.use(pathHandler);
+server.use(morgan("dev"));
 // // router
 // server.get("/", async (req, res) => {
 //   try {
