2a04fc67e8fe8432af91b4f4e7380e59dc0713dd
RoxGarciaRod <roxyjosimar97@gmail.com>
2024-04-21 20:23:46 -0300

    vista real funciona, falta agregar orden a las cards

--- src/router/index.socket.js
+++ src/router/index.socket.js
@@ -2,9 +2,9 @@ import productManager from "../data/fs/ProductManager.fs.js"
 
 export default async (socket) => {
     console.log("Client id: " + socket.id)
-    socket.emit("real", await productManager.read())
+    socket.emit("products", await productManager.read())
     socket.on("register", async data => {
         await productManager.create(data)
-        socket.emit("real", await productManager.read())
+        socket.emit("products", await productManager.read())
     })
 }
