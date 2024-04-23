43a2a9a5b972ae6662b4b4d2dfd60170684911f5
RoxGarciaRod <roxyjosimar97@gmail.com>
2024-04-23 19:59:10 -0300

    correccion de rutas con nuevos hbs

--- src/router/views/users.view.js
+++ src/router/views/users.view.js
@@ -1,15 +1,8 @@
 import { Router } from "express";
-import usersManager from "../../data/fs/UserManager.fs.js";
+import userManager from "../../data/fs/UserManager.fs.js";
 
 const usersRouter = Router();
 
-// usersRouter.get("/real", async (req, res, next) => {
-//   try {
-//     return res.render("real", { title: "REAL" });
-//   } catch (error) {
-//     return next(error);
-//   }
-// });
 usersRouter.get("/register", async (req, res, next) => {
   try {
     return res.render("register", { title: "REGISTER" });
@@ -17,18 +10,19 @@ usersRouter.get("/register", async (req, res, next) => {
     return next(error);
   }
 });
-// usersRouter.get("/login", async (req, res, next) => {
-//   try {
-//     return res.render("login", { title: "LOGIN" });
-//   } catch (error) {
-//     return next(error);
-//   }
-// });
-usersRouter.get("/:uid", async (req, res, next) => {
+
+usersRouter.get("/", async (req, res, next) => {
   try {
-    const { uid } = req.params;
-    const profile = await usersManager.readOne(uid);
-    return res.render("profile", { title: "PROFILE" });
+    return res.render("login", { title: "LOGIN" });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+usersRouter.get("/", async (req, res, next) => {
+  try {
+   const users = await userManager.read();
+    return res.render("profile", { users });
   } catch (error) {
     return next(error);
   }
