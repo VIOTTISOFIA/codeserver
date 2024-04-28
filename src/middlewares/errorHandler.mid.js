fd53a588c36e438a4c70ef62a94df787a5ba318a
RoxGarciaRod <roxyjosimar97@gmail.com>
2024-04-27 21:55:06 -0300

    elimino archivos archivos creados por error en main

--- src/middlewares/errorHandler.mid.js
+++
@@ -1,7 +0,0 @@
-function errorHandler(error, req, res, next) {
-  return res.json({
-    statusCode: error.statusCode || 500,
-    message: error.message || "coder api ERROR",
-  });
-}
-export default errorHandler;
