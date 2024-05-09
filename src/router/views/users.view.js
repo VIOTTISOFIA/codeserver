653210c7368c63347f3bdc51cf3d06fb2479d770
Sofía Viotti <sofi_04_04@hotmail.com>
2024-05-09 18:31:59 -0400

    profile

--- src/router/views/users.view.js
+++ src/router/views/users.view.js
@@ -17,18 +17,26 @@ usersRouter.get("/register", async (req, res, next) => {
     return next(error);
   }
 });
-// usersRouter.get("/login", async (req, res, next) => {
-//   try {
-//     return res.render("login", { title: "LOGIN" });
-//   } catch (error) {
-//     return next(error);
-//   }
-// });
+usersRouter.get("/login", async (req, res, next) => {
+  try {
+    return res.render("login", { title: "LOGIN" });
+  } catch (error) {
+    return next(error);
+  }
+});
+usersRouter.get("/profile", async (req, res, next) => {
+  try {
+    const users = await userManager.read();
+    return res.render("profile", { users });
+  } catch (error) {
+    return next(error);
+  }
+});
 usersRouter.get("/:uid", async (req, res, next) => {
   try {
     const { uid } = req.params;
-    const profile = await userManager.readOne(uid);
-    return res.render("profile", { title: "REAL", profile });
+    const one = await userManager.readOne(uid);
+    return res.render("profile", { user: one });
   } catch (error) {
     return next(error);
   }
