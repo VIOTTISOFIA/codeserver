05880531c7fa55ce4119a88e57670f6bbd7884a4
Sofía Viotti <sofi_04_04@hotmail.com>
2024-05-20 08:15:51 -0400

    tickets2

--- src/router/api/index.api.js
+++ src/router/api/index.api.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import productRouter from "./products.api.js";
+import ticketsRouter from "./tickets.api.js";
 
 import cartsRouter from "./carts.api.js";
 import usersApi from "./users.api.js";
@@ -9,7 +10,7 @@ const apiRouter = Router();
 apiRouter.use("/carts", cartsRouter);
 
 apiRouter.use("/products", productRouter);
-
 apiRouter.use("/users", usersApi);
+apiRouter.use("/ticktes", ticketsRouter);
 
 export default apiRouter;
--- src/router/api/tickets.api.js
+++ src/router/api/tickets.api.js
@@ -1,5 +1,30 @@
 import { Router } from "express";
+import { Types } from "mongoose";
+
 import cartsManager from "../../data/mongo/managers/CartsManager.mongo.js";
+
 const ticketsRouter = Router();
+
+ticketsRouter.get("/:uid", async (req, res, next) => {
+  try {
+    const { uid } = req.params;
+    // console.log(`Received request for user ID: ${uid}`);
+    const tickets = await cartsManager.aggregate([
+      {
+        $match: {
+          user_id: new Types.ObjectId(uid),
+        },
+      },
+    ]);
+    // console.log(`Found tickets: ${JSON.stringify(tickets)}`);
+    return res.json({
+      statusCode: 200,
+      response: tickets,
+    });
+  } catch (error) {
+    // console.error(`Error processing request: ${error.message}`);
+    return next(error);
+  }
+});
 
 export default ticketsRouter;
