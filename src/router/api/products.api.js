d5060dcb980656720ee526c0f290d45c7e5ed682
RoxGarciaRod <roxyjosimar97@gmail.com>
2024-08-27 23:00:18 -0300

    joi en products.api

--- src/router/api/products.api.js
+++ src/router/api/products.api.js
@@ -2,6 +2,8 @@
 //import productManager from "../../data/fs/ProductManager.fs.js";
 //import productManager from "../../data/mongo/managers/ProductsManager.mongo.js";
 import isValidAdmin from "../../middlewares/isValidAdmin.mid.js";
+import validator from "../../middlewares/joi.mid.js";
+import productsSchema from "../../schemas/products.schema.js";
 import CustomRouter from "../customRouter.js";
 import { create, read, readOne, paginate, update, destroy } from "../../controllers/products.controller.js";
 
@@ -10,7 +12,7 @@ class ProductsRouter extends CustomRouter {
     this.read("/", ["PUBLIC"], read);
     this.read("/paginate", ["PUBLIC"], paginate);
     this.read("/:pid", ["PUBLIC"], readOne);
-    this.create("/", ["ADMIN"], isValidAdmin, create);
+    this.create("/", ["ADMIN"], isValidAdmin, validator(productsSchema), create);
     this.update("/:pid", ["ADMIN"], update);
     this.destroy("/:pid", ["ADMIN"], destroy);
   }
